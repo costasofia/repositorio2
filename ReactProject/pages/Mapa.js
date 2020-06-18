044e65681d0295de6e92add04384437b14e1323c
sofia99costa@gmail.com <sofia99costa@gmail.com>
2020-06-18 22:29:47 +0100

    Mapa versão1

--- ReactProject/pages/Mapa.js
+++ ReactProject/pages/Mapa.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import {View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, } from 'react-native';
 import { StackAActions, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-function Mapa({route, navigation}){
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-   const parametro = route.params.parametro;
- 
-   return (
-	<View>
-        <Text>
-	    {parametro}
-        </Text>
-	</View>
-   );
+function Mapa({ route, navigation }) {
+
+    const parametro = route.params.parametro;
+
+    return (
+        /*<View>
+            <Text>
+            {parametro}
+            </Text>
+        </View>*/
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}>
+            </MapView>
+        </View>
+
+    );
 }
+const styles = StyleSheet.create({
+  container: {
+      ...StyleSheet.absoluteFillObject,
+      height: '100%',
+  },
+  map:{
+      ...StyleSheet.absoluteFillObject,
+  },
+});
+
 export default Mapa;
